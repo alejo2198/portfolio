b3aea1a4c6827ca18f51cec8543f67a50f67a6d5
Alejandro Castro <alejo199821@gmail.com>
2023-08-25 11:52:27 -0400

    align portfolio image,include text mixin

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,26 @@
 import classes from './Header.module.scss'
 import logo from '../../assets/icons/logo.svg'
 import downloadIcon from '../../assets/icons/download.svg'
-
+import profilePicture from '../../assets/images/profile.png'
 const Header = () => {
     return (
         <header className={classes.header}>
-            <div className="logo">
+            <div className={classes.logo}>
                 <a href='/'><img src={logo} alt="logo for site. A desktop with a coding display" className={classes['logo__icon']} /></a>
-                <p className={classes['logo__icon']}>Junior Developer</p>
+                <p className={classes['logo__text']}>Junior Developer</p>
             </div>  
-            <h4 className="header__subTitle">Hello I'm</h4>
+            <div className={classes['header__content-container']}>
+                <h1 className={classes['header__title']}>
+                    Hello I'm <span className={classes['header__title--name']}>Alejandro Castro</span> A Friendly Developer
+                </h1>
+                <div className={classes['header__button-container']}>
+                    
+                </div>
+                <img src={profilePicture} alt="profile picture of Alejandro Castro" className={classes['header__profile-picture']} />
+            </div>
+            
+
+            
         </header>
     );
 };
