59b4454b62f149b820865bf17dfa8f34af2c6c60
Alejandro Castro <alejo199821@gmail.com>
2023-08-29 13:42:11 -0400

    complete mobile form styling.complete error states. position graphic.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header/Header';
 import AboutMe from './components/AboutMe/AboutMe';
 import TechList from './components/TechList/TechList';
 import ProjectList from './components/ProjectList/ProjectList';
+import Form from './components/Form/Form';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <AboutMe/>
         <TechList/>
         <ProjectList/>
+        <Form/>
       </main>
     </div>
     
---
+++ src/components/Form/Form.js
@@ -0,0 +1,25 @@
+import classes from './Form.module.scss'
+import FormGraphic from '../../assets/images/form-graphic.svg'
+
+const Form = () => {
+    return (
+        <section className={classes["form-section"]}>
+            <h2 className={classes["form__title"]}>Contact Me</h2>
+            <p className={classes["form__text"]}>If you feel like I would be a good addition to your team, please don’t hesitate to reach out. I would love to hear from you.</p>
+            <img className={classes["form__img"]} src={FormGraphic} alt="" />
+            <form className={classes["form"]}>
+                <label className={classes["form__label"]} htmlFor="full-name">Full Name:</label>
+                <input className={classes["form__input"]} type="text" name="full-name" />
+                <label className={classes["form__label"]} htmlFor="company">Company:</label>
+                <input  className={classes["form__input"]} type="text" name="company"  />
+                <label className={classes["form__label"]} htmlFor="email">Email:</label>
+                <input  className={classes["form__input"]} type="email" name="email"/>
+                <label className={classes["form__label"]} htmlFor="message">Message:</label>
+                <textarea className={classes["form__textarea"]} name="message" ></textarea>
+                <button className={classes["form__button"]}>SEND</button>
+            </form>
+        </section>
+    );
+};
+
+export default Form;
