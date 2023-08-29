27760f0a830d23df914c4c5f8891b18436d95147
Alejandro Castro <alejo199821@gmail.com>
2023-08-29 17:44:44 -0400

    create modal, complete styling, move modal using a portal

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,13 +1,27 @@
 import classes from './Form.module.scss'
 import FormGraphic from '../../assets/images/form-graphic.svg'
+import SuccessModal from '../SuccessModal/SuccessModal';
 
+import { useState } from 'react';
 const Form = () => {
+
+    const [isModalOpen,setIsModalOpen] = useState(false)
+
+    const openModal = (event) => {
+        event.preventDefault();
+        setIsModalOpen(true)
+    }
+    const closeModal = (event) => {
+        event.preventDefault();
+        setIsModalOpen(false)
+    }
     return (
         <section className={classes["form-section"]}>
+            {isModalOpen && <SuccessModal closeModal={closeModal}/>}
             <h2 className={classes["form__title"]}>Contact Me</h2>
             <p className={classes["form__text"]}>If you feel like I would be a good addition to your team, please don’t hesitate to reach out. I would love to hear from you.</p>
             <img className={classes["form__img"]} src={FormGraphic} alt="" />
-            <form className={classes["form"]}>
+            <form className={classes["form"]} onSubmit={openModal}>
                 <label className={classes["form__label"]} htmlFor="full-name">Full Name:</label>
                 <input className={classes["form__input"]} type="text" name="full-name" />
                 <label className={classes["form__label"]} htmlFor="company">Company:</label>
---
+++ src/components/SuccessModal/SuccessModal.js
@@ -0,0 +1,30 @@
+import classes from './SuccessModal.module.scss'
+
+import closeIcon from '../../assets/icons/close_round.svg'
+import checkmarkIcon from '../../assets/icons/checkmark.svg'
+
+import ReactDOM  from 'react-dom';
+
+const Modal = ({closeModal}) => {
+    return (
+        <div className={classes.overlay} onClick={closeModal}>
+<           div className={classes.modal} onClick={e => e.stopPropagation()}>
+                <img className={classes['modal__close-icon']} src={closeIcon} alt="a rounded x" onClick={closeModal}/>
+                <img className={classes['modal__checkmark-icon']} src={checkmarkIcon} alt="a checkmark" />
+                <h2 className={classes['modal__title']}>Success</h2>
+                <p className={classes['modal__text']}>Thank you! Your message has been received. I will contact you in 1-2 business days. </p>
+            </div>
+        </div>
+        
+    );
+};
+
+const SuccessModal = ({closeModal}) => {
+    return (
+        <>
+            {ReactDOM.createPortal(<Modal closeModal={closeModal}/>,document.getElementById('modal-root'))}
+        </>
+    )
+}
+
+export default SuccessModal;
