f20fb29b350ac4e7e4d3272bde7d6705972e1154
Alejandro Castro <alejo199821@gmail.com>
2023-09-12 14:19:18 -0400

    update font size,layout and icon sizes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,13 +8,18 @@ import githubIcon from '../../assets/icons/footer-icons/github.svg'
 const Footer = () => {
     return (
         <footer className={classes.footer}>
-            <h4 className={classes['footer__title']}>Thank You for Visiting</h4>
-            <p className={classes['footer__text']}>I appreciate the time you took out of your day to check out my site. </p>
-            <ul className={classes['footer__links-container']}>
-                <li className={classes['footer__link-container']}><a className={classes['footer__link']} href="/about">About</a></li>
-                <li className={classes['footer__link-container']}><a className={classes['footer__link']} href="/projects">Projects</a></li>
-                <li className={classes['footer__link-container']}><a className={classes['footer__link']} href="/contact'">Contact Me</a></li >
-            </ul>
+            <div className={classes['layout-container']}>
+                <div className={classes['layout-container-text']}>
+                    <h4 className={classes['footer__title']}>Thank You for Visiting</h4>
+                    <p className={classes['footer__text']}>I appreciate the time you took out of your day to check out my site. </p>
+                </div>
+                <ul className={classes['footer__links-container']}>
+                    <li className={classes['footer__link-container']}><a className={classes['footer__link']} href="/about">About</a></li>
+                    <li className={classes['footer__link-container']}><a className={classes['footer__link']} href="/projects">Projects</a></li>
+                    <li className={classes['footer__link-container']}><a className={classes['footer__link']} href="/contact'">Contact Me</a></li >
+                </ul>
+            </div>
+            
             <ul className={classes['footer__icon-list']}>
                 <li className={classes['footer__icon-container']}><img className={classes['footer__icon']} src={resumeIcon} alt="resume download" /></li>
                 <li className={classes['footer__icon-container']}><img className={classes['footer__icon']} src={indeedIcon} alt="indeed link" /></li>
